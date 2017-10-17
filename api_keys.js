307fa41f6124e012ab3fbcb651119c59084bff30
Sumanth Reddy <srksumanth@gmail.com>
2017-10-17 06:58:26 +0530

    Update api_keys.js

--- api_keys.js
+++ api_keys.js
@@ -1,11 +1,11 @@
 var api = {
     google:{
-        clientID: "614895343008-6pu8i5ol16lf2bpcc6casf3qasbajrjr.apps.googleusercontent.com",
-        clientSecret: "RvcCxJZMO7u7YTozQXzu49m2"
+        clientID: "add your google key here",
+        clientSecret: "secret"
     },
     facebook:{
-        clientID:"827674830740209",
-        clientSecret:"e74993881cf91f85a361ad51c650e005"
+        clientID:"id",
+        clientSecret:"secret"
     }
 };
 module.exports = api;
